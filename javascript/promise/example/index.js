94cf3bbd9994aa30baae7e686a4aa1a232651591
zyb8023 <fengying11734@126.com>
2021-07-19 23:57:43 +0800

    promise

--- javascript/promise/example/index.js
+++ javascript/promise/example/index.js
@@ -1,7 +1,33 @@
-const p = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('ok');
-  }, 1000);
+const MyPromise = require('../promise');
+
+const p = new MyPromise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('ok');
+    }, 2002);
 });
 
-p.then(res => console.log(res));
+// p.then(res => {
+//     console.log(11111);
+//     console.log(res, 'res1');
+// })
+
+// p.then(res => {
+//     console.log(2222222);
+//     console.log(res, 'res2');
+// })
+
+p.then(res => {
+    console.log(3333333);
+    console.log(res, 'res3');
+}).then(res => {
+
+    console.log(res, '-----res');
+}).then(res => {
+    console.log(5555555);
+})
+
+
+// const p1 = p.then(res => {
+//     console.log(res);
+//     return p1;
+// })
