ffa2955e268a3ff4750becf0b27f3b359ee93428
zyb11734 <zyb11734@163.com>
2021-07-21 18:40:08 +0800

    暂存

--- javascript/promise/example/index.js
+++ javascript/promise/example/index.js
@@ -1,33 +1,13 @@
 const MyPromise = require('../promise');
 
 const p = new MyPromise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('ok');
-    }, 2002);
+  setTimeout(() => {
+    resolve('ok');
+  }, 2000);
 });
 
-// p.then(res => {
-//     console.log(11111);
-//     console.log(res, 'res1');
-// })
-
-// p.then(res => {
-//     console.log(2222222);
-//     console.log(res, 'res2');
-// })
-
 p.then(res => {
-    console.log(3333333);
-    console.log(res, 'res3');
+  console.log(res, '------res');
 }).then(res => {
-
-    console.log(res, '-----res');
-}).then(res => {
-    console.log(5555555);
+  console.log(222222222);
 })
-
-
-// const p1 = p.then(res => {
-//     console.log(res);
-//     return p1;
-// })
